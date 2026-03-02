a3fa1e625c56832b9677e0158662606813b44b73
sergiosebs2 <159736850+sergiosebs2@users.noreply.github.com>
2026-03-02 20:00:43 -0300

    Add files via upload

---
+++ ClasesdeDominio/Articulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesDeDominio
+{
+    public class Articulo
+    {
+        public int id { get; set; }
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public Marca marca { get; set; } = new Marca();    //CHAT GPT ME RECOMENDO ESTO
+        public Categoria categoria { get; set; } = new Categoria();     //CHAT GPT ME RECOMENDO ESTO, INVOCAR AL CONSTRUCTOR
+
+        public List<Imagenes> listImagenes { get; set; } //usamos para cargar mas de 1 imagen.-
+
+
+        public Imagenes imagenArticulo { get; set; } // se agrega get y set para que se logre Modificar .-D
+        public float precio { get; set; }
+
+        public Articulo() {;}
+
+    }
+}
---
+++ ClasesdeDominio/Imagenes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesDeDominio
+{
+    public class Imagenes
+    {
+        public int id { get; set; }
+        public string urlImagen { get; set; }
+        public int idArticulo { get; set; }
+
+        public Imagenes(int idA, string urlImag) { 
+            this.idArticulo = idA;
+            this.urlImagen = urlImag;
+        }
+        public override string ToString()
+        {
+            return urlImagen;
+        }
+
+    }
+}
